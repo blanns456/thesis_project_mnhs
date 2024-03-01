366ef27146b586cb17fe22233cf7f8493349ac59
jayveeroda <rodajohnvincent35@gmail.com>
2024-03-01 16:16:15 +0800

    jhs enrollment

--- frontend_mnhs/src/app/jhs-enrollment/jhs-enrollment.component.ts
+++ frontend_mnhs/src/app/jhs-enrollment/jhs-enrollment.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import SignaturePad from 'signature_pad';
 import { ApiControllers } from '../controllers/controller.component';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 declare var swal: any;
 
 @Component({
@@ -13,6 +14,9 @@ declare var swal: any;
 export class JhsEnrollmentComponent implements OnInit {
   imageURL: string | undefined;
   studata: any;
+  myForm: any;
+  isYesSelected: boolean = false;
+  isPantawidSelected: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -27,37 +31,62 @@ export class JhsEnrollmentComponent implements OnInit {
   // formControlName = 'password';
 
   enrollForm: FormGroup = this.formBuilder.group({
-    profile: ['', [Validators.required]],
+    gradelevel: ['', [Validators.required]],
+    program: ['', [Validators.required]],
+    lrn: ['', [Validators.required]],
     firstname: ['', [Validators.required]],
     lastname: ['', [Validators.required]],
     middlename: [''],
-    gender: ['', [Validators.required]],
     suffix: [''],
-    civil_status: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
     birthdate: ['', [Validators.required]],
+    age: [''],
     birth_place: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    mobile_number: ['', [Validators.required]],
-    citizenship: ['', [Validators.required]],
-    religion: ['', [Validators.required]],
     home_address: ['', [Validators.required]],
     present_address: ['', [Validators.required]],
-    gradelevel: ['', [Validators.required]],
-    program: ['', [Validators.required]],
+    m_tounge: [''],
+    email: ['', [Validators.required]],
+    mobile_number: ['', [Validators.required]],
+    ip: ['', Validators.required],
+    pantawid: ['', [Validators.required]],
     school_elem: ['', [Validators.required]],
     school_schoolyr: ['', [Validators.required]],
-    mother_name: ['', [Validators.required]],
-    father_name: ['', [Validators.required]],
-    mother_occupation: ['', [Validators.required]],
-    father_occupation: ['', [Validators.required]],
-    mother_contactnumber: ['', [Validators.required]],
-    father_contactnumber: ['', [Validators.required]],
+    father_lastName: [''],
+    father_firstName: [''],
+    father_middleName: [''],
+    father_number: [''],
+    mother_lastName: [''],
+    mother_firstName: [''],
+    mother_middleName: [''],
+    mother_number: [''],
+    guardian_lastName: [''],
+    guardian_firstName: [''],
+    guardian_middleName: [''],
+    guardian_number: [''],
+    profile: ['', [Validators.required]],
     signature: ['', [Validators.required]],
   });
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.signaturePad = new SignaturePad(this.canvasEl.nativeElement);
+  }
+
+  calculateAge() {
+    const birthdate = this.enrollForm?.get('birthdate')?.value;
+    if (birthdate) {
+      const birthdateDate = new Date(birthdate);
+      const today = new Date();
+      let age = today.getFullYear() - birthdateDate.getFullYear();
+      const monthDiff = today.getMonth() - birthdateDate.getMonth();
+      if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthdateDate.getDate())
+      ) {
+        age--;
+      }
+      this.enrollForm?.get('age')?.setValue(age);
+    }
   }
 
   startDrawing(event: Event) {
@@ -89,6 +118,7 @@ export class JhsEnrollmentComponent implements OnInit {
       'imagefilename',
       this.enrollForm.controls['profile'].value
     );
+    submitdata.append('lrn', this.enrollForm.controls['lrn'].value);
     submitdata.append('firstname', this.enrollForm.controls['firstname'].value);
     submitdata.append('lastname', this.enrollForm.controls['lastname'].value);
     submitdata.append(
@@ -97,29 +127,23 @@ export class JhsEnrollmentComponent implements OnInit {
     );
     submitdata.append('gender', this.enrollForm.controls['gender'].value);
     submitdata.append('suffix', this.enrollForm.controls['suffix'].value);
-    submitdata.append(
-      'civil_status',
-      this.enrollForm.controls['civil_status'].value
-    );
     submitdata.append('birthdate', this.enrollForm.controls['birthdate'].value);
     submitdata.append(
       'birth_place',
       this.enrollForm.controls['birth_place'].value
     );
+    submitdata.append('age', this.enrollForm.controls['age'].value);
     submitdata.append('email', this.enrollForm.controls['email'].value);
     submitdata.append(
       'mobile_number',
       this.enrollForm.controls['mobile_number'].value
     );
     submitdata.append(
-      'citizenship',
-      this.enrollForm.controls['citizenship'].value
-    );
-    submitdata.append('religion', this.enrollForm.controls['religion'].value);
-    submitdata.append(
       'home_address',
       this.enrollForm.controls['home_address'].value
     );
+    submitdata.append('ip', this.enrollForm.controls['ip'].value);
+    submitdata.append('pantawid', this.enrollForm.controls['pantawid'].value);
     submitdata.append(
       'present_address',
       this.enrollForm.controls['present_address'].value
@@ -137,73 +161,94 @@ export class JhsEnrollmentComponent implements OnInit {
       'school_schoolyr',
       this.enrollForm.controls['school_schoolyr'].value
     );
-    submitdata.append(
-      'mother_name',
-      this.enrollForm.controls['mother_name'].value
-    );
-    submitdata.append(
-      'father_name',
-      this.enrollForm.controls['father_name'].value
-    );
-    submitdata.append(
-      'mother_occupation',
-      this.enrollForm.controls['mother_occupation'].value
-    );
-    submitdata.append(
-      'father_occupation',
-      this.enrollForm.controls['father_occupation'].value
-    );
-    submitdata.append(
-      'mother_contactnumber',
-      this.enrollForm.controls['mother_contactnumber'].value
-    );
-    submitdata.append(
-      'father_contactnumber',
-      this.enrollForm.controls['father_contactnumber'].value
-    );
     submitdata.append('signature', this.enrollForm.controls['signature'].value);
+    submitdata.append(
+      'father_lastName',
+      this.enrollForm.controls['father_lastName'].value
+    );
+    submitdata.append(
+      'father_firstName',
+      this.enrollForm.controls['father_firstName'].value
+    );
+    submitdata.append(
+      'father_middleName',
+      this.enrollForm.controls['father_middleName'].value
+    );
+    submitdata.append(
+      'father_number',
+      this.enrollForm.controls['father_number'].value
+    );
+    submitdata.append(
+      'mother_lastName',
+      this.enrollForm.controls['mother_lastName'].value
+    );
+    submitdata.append(
+      'mother_firstName',
+      this.enrollForm.controls['mother_firstName'].value
+    );
+    submitdata.append(
+      'mother_middleName',
+      this.enrollForm.controls['mother_middleName'].value
+    );
+    submitdata.append(
+      'mother_number',
+      this.enrollForm.controls['mother_number'].value
+    );
+    submitdata.append(
+      'guardian_lastName',
+      this.enrollForm.controls['guardian_lastName'].value
+    );
+    submitdata.append(
+      'guardian_firstName',
+      this.enrollForm.controls['guardian_firstName'].value
+    );
+    submitdata.append(
+      'guardian_middleName',
+      this.enrollForm.controls['guardian_middleName'].value
+    );
+    submitdata.append(
+      'guardian_number',
+      this.enrollForm.controls['guardian_number'].value
+    );
 
     if (
       this.enrollForm.controls['profile'].invalid ||
       this.enrollForm.controls['firstname'].invalid ||
       this.enrollForm.controls['lastname'].invalid ||
       this.enrollForm.controls['gender'].invalid ||
-      this.enrollForm.controls['civil_status'].invalid ||
+      this.enrollForm.controls['lrn'].invalid ||
       this.enrollForm.controls['birthdate'].invalid ||
       this.enrollForm.controls['birth_place'].invalid ||
       this.enrollForm.controls['email'].invalid ||
       this.enrollForm.controls['mobile_number'].invalid ||
-      this.enrollForm.controls['citizenship'].invalid ||
-      this.enrollForm.controls['religion'].invalid ||
+      this.enrollForm.controls['ip'].invalid ||
+      this.enrollForm.controls['pantawid'].invalid ||
       this.enrollForm.controls['home_address'].invalid ||
       this.enrollForm.controls['present_address'].invalid ||
       this.enrollForm.controls['gradelevel'].invalid ||
       this.enrollForm.controls['program'].invalid ||
       this.enrollForm.controls['school_elem'].invalid ||
       this.enrollForm.controls['school_schoolyr'].invalid ||
-      this.enrollForm.controls['mother_name'].invalid ||
-      this.enrollForm.controls['father_name'].invalid ||
-      this.enrollForm.controls['mother_occupation'].invalid ||
-      this.enrollForm.controls['father_occupation'].invalid ||
-      this.enrollForm.controls['mother_contactnumber'].invalid ||
-      this.enrollForm.controls['father_contactnumber'].invalid ||
       this.enrollForm.controls['signature'].invalid
     ) {
-      console.log('hello');
+      // console.log(this.enrollForm.value);
       return;
     }
     this.apiController.createstudent(submitdata).subscribe((e) => {
       this.studata = e;
       if (this.studata['user'] == 'success') {
-        swal({
+        Swal.fire({
           title: 'Success',
-          type: 'success',
-          confirmButtonText: 'Okay',
+          text: 'Please Check Your Emnail',
+          icon: 'success',
         }).then(function () {
           console.log('success');
         });
 
         console.log(e);
+      } else {
+        console.log(this.enrollForm.value);
+        console.log(this.studata['message']);
       }
     });
   }
