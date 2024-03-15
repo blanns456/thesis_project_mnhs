094dc876d1907f29c95721cc7665555ac9af9124
JBHtzy <jamesbadang16@gmail.com>
2024-03-15 14:26:39 -0700

    login role

--- frontend_mnhs/src/app/login/login.component.ts
+++ frontend_mnhs/src/app/login/login.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { data } from 'jquery';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(): void {
     if (!this.username || !this.password) {
@@ -24,18 +25,32 @@ export class LoginComponent {
       return;
     }
 
-    this.http.post<any>('http://127.0.0.1:8000/api/login', { username: this.username, password: this.password })
+    this.http
+      .post<any>('http://127.0.0.1:8000/api/login', {
+        username: this.username,
+        password: this.password,
+      })
       .subscribe(
-        response => {
+        (response) => {
           localStorage.setItem('token', response.token);
-          Swal.fire({
-            title: 'Hi',
-            text: 'Welcome',
-            icon: 'success',
-          });
-          this.router.navigate(['student/home']);
+          // console.log(response);
+          if (response.role === 2) {
+            this.router.navigate(['student/home']);
+            Swal.fire({
+              title: 'Hi',
+              text: 'Welcome',
+              icon: 'success',
+            });
+          } else if (response.role === 1) {
+            this.router.navigate(['admin/home']);
+            Swal.fire({
+              title: 'Hi',
+              text: 'Welcome Admin',
+              icon: 'success',
+            });
+          }
         },
-        error => {
+        (error) => {
           Swal.fire({
             title: 'Error',
             text: 'Invalid username or password.',
